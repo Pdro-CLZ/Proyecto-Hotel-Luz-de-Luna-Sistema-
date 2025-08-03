fe9c61cafd4efc4492833006c972b91224f5df0a
Pedro Centeno Lopez <220105694+Pdro-CLZ@users.noreply.github.com>
2025-08-03 15:40:04 -0600

    Sistema de hotel
    De aqui en adelante hay que cambiar el modulo que le toca a cada uno

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    if (loginForm) {
+        loginForm.addEventListener('submit', function(e) {
+            e.preventDefault();
+            window.location.href = "menu.html";
+        });
+    }
+});
